a4e3de494b3054fd4f6dccbd3927ea5b7b3e7c2f
sanjeevmax6 <sanvegas2001@gmail.com>
2021-11-24 11:07:50 +0530

    Fixed Prediction error

--- src/index.py
+++ src/index.py
@@ -8,17 +8,27 @@ def data():
     X_train, Y_train, X_test, Y_test = preProcess(cpu_data, memory_data, disk_data)
     return X_train, Y_train, X_test, Y_test
 
+Network = NeuralNetwork()
+
 def train(X_train, Y_train, X_test, Y_test):
-    Network = NeuralNetwork()
-    for i in range(len(X_train)):
-        Network.fit(X_train[i], Y_train[i])
+    # Network = NeuralNetwork()
+    # for i in range(len(X_train)):
+    Network.fit(X_train[0], Y_train[0])
     return Network
 
 def predict(Network, data):
     pred = Network.predict(data)
     return pred
 
+def accuracy(Network, y, yhat):
+    accuracy = Network.acc(y, yhat)
+    return accuracy
+
 X_train, Y_train, X_test, Y_test = data()
 Net = train(X_train, Y_train, X_test, Y_test)
+# Net.returnWeights()
+# print(X_test[0].shape)
 predicted = predict(Net, X_test[0])
 print(predicted)
+# acc = accuracy(Net, Y_test[0], predicted)
+# print(acc)
